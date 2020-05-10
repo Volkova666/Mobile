ce42458e99f8798789b96c5ed8d08e87d3ed3c90
Volkova666 <63210055+Volkova666@users.noreply.github.com>
2020-05-10 10:53:11 +0300

    Add files via upload

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(157, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(380, 22);
+            this.textBox3.TabIndex = 35;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(89, 162);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(448, 22);
+            this.textBox9.TabIndex = 34;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(383, 134);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(154, 22);
+            this.textBox8.TabIndex = 33;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(89, 134);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(218, 22);
+            this.textBox7.TabIndex = 32;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(89, 186);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(448, 22);
+            this.textBox6.TabIndex = 31;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(213, 98);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(324, 22);
+            this.textBox5.TabIndex = 30;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(448, 22);
+            this.textBox4.TabIndex = 29;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(89, 285);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 28;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(12, 283);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 26);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Дата:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(12, 35);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 26);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Сч №:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(12, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 26);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Сч №:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 26);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "БИК:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(313, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 26);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "КПП:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 26);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "ИНН:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 26);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Банк получателя:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 26);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Получатель:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(110, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(394, 48);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Составить счёт";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 26);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Сч №:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 22);
+            this.textBox1.TabIndex = 37;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private readonly string TemplateFileName = "eyx.docx"; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var name = textBox3.Text;
+            var billtake = textBox4.Text;
+            var bank = textBox5.Text;
+            var inn = textBox7.Text;
+            var kpp = textBox8.Text;
+            var bik = textBox9.Text;
+            var bill = textBox6.Text;
+            var date = dateTimePicker2.Value.ToShortDateString();
+            var results = Convert.ToString (Math.Round((Lab1.Calculate()),2));
+            var result = Convert.ToString(Math.Round((Lab2.Calculate()),2));
+            var all = Convert.ToString (Math.Round ((Lab1.Calculate() + Lab2.Calculate()),2));
+            var a = Lab1.Calculate() + Lab2.Calculate();
+            var nds = Convert.ToString(Math.Round((a * 0.2), 2));
+            var b = Convert.ToString(Math.Round((a * 1.2), 2));
+            var num = textBox1.Text;
+
+
+
+            var wordApp = new Word.Application();
+            wordApp.Visible = false;
+
+            var wordDoc = wordApp.Documents.Open(TemplateFileName);
+            ReplaceWordStub("{name}", name, wordDoc);
+            ReplaceWordStub("{billtake}", billtake, wordDoc);
+            ReplaceWordStub("{bank}", bank, wordDoc);
+            ReplaceWordStub("{inn}", inn, wordDoc);
+            ReplaceWordStub("{kpp}", kpp, wordDoc);
+            ReplaceWordStub("{bik}", bik, wordDoc);
+            ReplaceWordStub("{bill}", bill, wordDoc);
+            ReplaceWordStub("{date}", date, wordDoc);
+            ReplaceWordStub("{name}", name, wordDoc);
+            ReplaceWordStub("{results}", results, wordDoc);
+            ReplaceWordStub("{result}", result, wordDoc);
+            ReplaceWordStub("{results}", results, wordDoc);
+            ReplaceWordStub("{result}", result, wordDoc);
+            ReplaceWordStub("{all}", all, wordDoc);
+            ReplaceWordStub("{result}", result, wordDoc);
+            ReplaceWordStub("{nds}", nds, wordDoc);
+            ReplaceWordStub("{b}", b, wordDoc);
+            ReplaceWordStub("{num}", num, wordDoc);
+
+
+            wordDoc.SaveAs("C:\result.docx"); 
+            wordDoc.ExportAsFixedFormat("C:\result.pdf", Word.WdExportFormat.wdExportFormatPDF); 
+
+            wordApp.Visible = true;
+        }
+        
+        private void ReplaceWordStub(string stubToReplace, string text, Word.Document wordDoc)
+        {
+            var range = wordDoc.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: stubToReplace, ReplaceWith: text);
+
+        }
+    }
+    class Lab1
+    {
+        public static float Calculate()
+        {
+            var data = ProcessCSV("data.csv");     
+            float costt = 0;
+            float costs = 0;
+            foreach (var ndata in data)
+            {
+                int result = String.Compare(ndata.msisdn_origin, Cost.num);
+                if (result == 0)
+                {
+                    string x = ndata.call_duration;
+                    x = x.Replace('.', ',');
+                    float d = Convert.ToSingle(x);
+                    costt = (d * Cost.origin) - Cost.free;
+                    string b = ndata.sms_number;
+                    costs = Convert.ToSingle(b) * Cost.sms;
+                }
+            }
+            float results = costt + costs;
+            return results;
+        }
+        private static List<Data> ProcessCSV(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(row => row.Length > 0)
+                .Select(Data.ParseRow).ToList();
+        }
+    }
+    public class Data
+    {
+        public string timestamp { get; set; }
+        public string msisdn_origin { get; set; }
+        public string msisdn_dest { get; set; }
+        public string call_duration { get; set; }
+        public string sms_number { get; set; }
+
+        internal static Data ParseRow(string row)
+        {
+            var columns = row.Split(',');
+
+            return new Data()
+            {
+                timestamp = columns[0],
+                msisdn_origin = columns[1],
+                msisdn_dest = columns[2],
+                call_duration = columns[3],
+                sms_number = columns[4]
+            };
+        }
+    }
+    public class Cost
+    {
+        internal const float origin = 2;
+        internal const float sms = 2;
+        internal const float free = 40;
+        internal const string num = "915783624";
+        internal const float internet = 1;
+        internal const string ip = "192.168.250.27";
+    }
+    class Lab2
+    {
+        public static float Calculate()
+        {
+            var data = ProcessCSV("data1.csv"); 
+            float costtI = 0;
+            float costtO = 0;
+            foreach (var ndata in data)
+            {
+                int result1 = String.Compare(ndata.Sa, Cost.ip);
+                int result2 = String.Compare(ndata.Da, Cost.ip);
+                if (result1 == 0 || result2 == 0)
+                {
+                    float Ibyt = Convert.ToSingle(ndata.Ibyt);
+                    float Obyt = Convert.ToSingle(ndata.Obyt);
+                    costtI = (costtI + Ibyt);
+                    costtO = (costtO + Obyt);
+                }
+            }
+            float costt = (costtI + costtO) / (1024 * 1024) * Cost.internet;
+            return costt;
+        }
+        private static List<Data2> ProcessCSV(string path)
+        {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(row => row.Length > 0)
+                .Select(Data2.ParseRow).ToList();
+        }
+    }
+    public class Data2
+    {
+        public string Ts { get; set; }
+        public string Te { get; set; }
+        public string Td { get; set; }
+        public string Sa { get; set; }
+        public string Da { get; set; }
+        public string Sp { get; set; }
+        public string Dp { get; set; }
+        public string Pr { get; set; }
+        public string Ra_flg { get; set; }
+        public string Fwd { get; set; }
+        public string Stos { get; set; }
+        public string Ipkt { get; set; }
+        public string Ibyt { get; set; }
+        public string Opkt { get; set; }
+        public string Obyt { get; set; }
+        public string In_ { get; set; }
+        public string Out_ { get; set; }
+        public string Sas { get; set; }
+        public string Das { get; set; }
+        public string Smk { get; set; }
+        public string Dmk { get; set; }
+        public string Dtos { get; set; }
+        public string Dir { get; set; }
+        public string Nh { get; set; }
+        public string Nhb { get; set; }
+        public string Svln { get; set; }
+        public string Dvln { get; set; }
+        public string Ismc { get; set; }
+        public string Odmc { get; set; }
+        public string Idmc { get; set; }
+        public string Osmc { get; set; }
+        public string Mpls1 { get; set; }
+        public string Mpls2 { get; set; }
+        public string Mpls3 { get; set; }
+        public string Mpls4 { get; set; }
+        public string Mpls5 { get; set; }
+        public string Mpls6 { get; set; }
+        public string Mpls7 { get; set; }
+        public string Mpls8 { get; set; }
+        public string Mpls9 { get; set; }
+        public string Mpls10 { get; set; }
+        public string Cl { get; set; }
+        public string Sl { get; set; }
+        public string Al { get; set; }
+        public string Ra { get; set; }
+        public string Eng { get; set; }
+        public string Exid { get; set; }
+        public string Tr { get; set; }
+
+        internal static Data2 ParseRow(string row)
+        {
+            var columns = row.Split(';');
+
+            return new Data2()
+            {
+                Ts = columns[0],
+                Te = columns[1],
+                Td = columns[2],
+                Sa = columns[3],
+                Da = columns[4],
+                Sp = columns[5],
+                Dp = columns[6],
+                Pr = columns[7],
+                Ra_flg = columns[8],
+                Fwd = columns[9],
+                Stos = columns[10],
+                Ipkt = columns[11],
+                Ibyt = columns[12],
+                Opkt = columns[13],
+                Obyt = columns[14],
+                In_ = columns[15],
+                Out_ = columns[16],
+                Sas = columns[17],
+                Das = columns[18],
+                Smk = columns[19],
+                Dmk = columns[20],
+                Dtos = columns[21],
+                Dir = columns[22],
+                Nh = columns[24],
+                Nhb = columns[24],
+                Svln = columns[25],
+                Dvln = columns[26],
+                Ismc = columns[27],
+                Odmc = columns[28],
+                Idmc = columns[29],
+                Osmc = columns[30],
+                Mpls1 = columns[31],
+                Mpls2 = columns[32],
+                Mpls3 = columns[33],
+                Mpls4 = columns[34],
+                Mpls5 = columns[35],
+                Mpls6 = columns[36],
+                Mpls7 = columns[37],
+                Mpls8 = columns[38],
+                Mpls9 = columns[39],
+                Mpls10 = columns[40],
+                Cl = columns[41],
+                Sl = columns[42],
+                Al = columns[43],
+                Ra = columns[44],
+                Eng = columns[45],
+                Exid = columns[46],
+                Tr = columns[47]
+            };
+        }
+    }
+}
